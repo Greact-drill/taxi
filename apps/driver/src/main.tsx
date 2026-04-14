e74510ec33460d636d6cc7b08e145edaacb1d057
Nikita Bizyaev <NBizyaev@alabuga.ru>
2026-04-14 13:52:01 +0300

    themes

--- apps/driver/src/main.tsx
+++ apps/driver/src/main.tsx
@@ -1,11 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
+import { ChakraProvider, createSystem, defaultConfig, defineConfig } from "@chakra-ui/react"
 import App from './App';
+
+const config = defineConfig({
+  globalCss: {
+    html: {
+      colorPalette: "purple",
+    },
+  },
+})
+
+export const system = createSystem(defaultConfig, config)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider value={system}>
       <App />
     </ChakraProvider>
   </StrictMode>
