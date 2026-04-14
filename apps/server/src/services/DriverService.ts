0f14113f81b3d0a4d4f689a16cf00d15dfb7d334
Nikita Bizyaev <NBizyaev@alabuga.ru>
2026-04-14 16:35:54 +0300

    test data

--- apps/server/src/services/DriverService.ts
+++ apps/server/src/services/DriverService.ts
@@ -70,16 +70,17 @@ export class DriverService {
 
   async bootstrap(): Promise<void> {
     const seeds: DriverCreateInput[] = [
-      { name: 'Иван Петров', car: 'Kia Rio · A111AA77', login: 'driver1', password: '1' },
-      { name: 'Сергей Козлов', car: 'VW Polo · B222BB77', login: 'driver2', password: '2' },
-      { name: 'Алексей Смирнов', car: 'Skoda Rapid · C333CC77', login: 'driver3', password: '3' },
-      { name: 'Дмитрий Волков', car: 'Renault Logan · E444EE77', login: 'driver4', password: '4' },
-      { name: 'Андрей Новиков', car: 'Lada Vesta · K555KK77', login: 'driver5', password: '5' },
-      { name: 'Михаил Орлов', car: 'Hyundai Solaris · M666MM77', login: 'driver6', password: '6' },
-      { name: 'Николай Соколов', car: 'Toyota Camry · O777OO77', login: 'driver7', password: '7' },
-      { name: 'Павел Лебедев', car: 'Kia Optima · P888PP77', login: 'driver8', password: '8' },
-      { name: 'Егор Морозов', car: 'Mazda 6 · T999TT77', login: 'driver9', password: '9' },
-      { name: 'Константин Егоров', car: 'Haval Jolion · X000XX77', login: 'driver10', password: '10' },
+      { name: 'Иван Петров', car: 'Kia Rio · а111аа77', login: 'driver1', password: '1' },
+      { name: 'Сергей Козлов', car: 'VW Polo · в222вв77', login: 'driver2', password: '2' },
+      { name: 'Алексей Смирнов', car: 'Skoda Rapid · с333сс77', login: 'driver3', password: '3' },
+      { name: 'Дмитрий Волков', car: 'Renault Logan · е444ее77', login: 'driver4', password: '4' },
+      { name: 'Андрей Новиков', car: 'Lada Vesta · к555кк77', login: 'driver5', password: '5' },
+      { name: 'Михаил Орлов', car: 'Hyundai Solaris · м666мм77', login: 'driver6', password: '6' },
+      { name: 'Николай Соколов', car: 'Toyota Camry · о777оо77', login: 'driver7', password: '7' },
+      { name: 'Павел Лебедев', car: 'Kia Optima · р888рр77', login: 'driver8', password: '8' },
+      { name: 'Егор Морозов', car: 'Mazda 6 · т999тт77', login: 'driver9', password: '9' },
+      { name: 'Константин Егоров', car: 'Haval Jolion · х000хх77', login: 'driver10', password: '10' },
+      { name: 'Альберт Шайхутдинов', car: 'Haval M6 · о031оа04', login: 'driver11', password: '11' },
     ];
     for (const input of seeds) {
       if (!(await this.findByLogin(input.login))) {
